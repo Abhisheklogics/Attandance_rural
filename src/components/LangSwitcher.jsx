fe4b523084495898295b36ba094e8eda8cf2bae3
abhi.Logics <abhi.kumar252004@gmail.com>
2025-09-27 19:38:33 +0530

    al

--- src/components/LangSwitcher.jsx
+++ src/components/LangSwitcher.jsx
@@ -30,7 +30,8 @@ export default function LangSwitcher() {
       <option value="en">English</option>
       <option value="hi">हिंदी</option>
       <option value="pa">ਪੰਜਾਬੀ</option>
-
+      <option value="ur">اردو</option>
+      <option value="ta">தமிழ்</option>
     </select>
   );
 }
